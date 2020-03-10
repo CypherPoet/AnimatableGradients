2a86f68b0f4203fbc057917f26041caa2f026a48
CypherPoet <CypherPoet@gmail.com>
2020-03-10 03:12:24 -0500

    Allow for AnimatableAngularGradientModifier to be constructed with a single, full-span start angle.

--- Sources/View Modifiers/AnimatableAngularGradient/Shape+AnimatableAngularGradient.swift
+++ Sources/View Modifiers/AnimatableAngularGradient/Shape+AnimatableAngularGradient.swift
@@ -3,12 +3,37 @@ import SwiftUI
 
 extension Shape {
 
+    /// - Parameters:
+    ///   - fullSpanStartAngle: Setting this property will cause the gradient to being at
+    ///         the specified offset, and then span the entire circumference. (Default: `nil`)
     public func animatableAngularGradient (
         startColors: [UIColor],
         endColors: [UIColor],
         centerPoint: UnitPoint = .center,
-        startAngle: Angle,
-        endAngle: Angle,
+        fullSpanStartAngle: Angle,
+        completionPercentage: CGFloat
+    ) -> some View {
+        self.modifier(
+            AnimatableAngularGradientModifier(
+                baseShape: self,
+                startColors: startColors,
+                endColors: endColors,
+                centerPoint: centerPoint,
+                fullSpanStartAngle: fullSpanStartAngle,
+                completionPercentage: completionPercentage
+            )
+        )
+    }
+    
+    
+    
+    
+    public func animatableAngularGradient (
+        startColors: [UIColor],
+        endColors: [UIColor],
+        centerPoint: UnitPoint = .center,
+        startAngle: Angle = .zero,
+        endAngle: Angle = .radians(2 * .pi),
         completionPercentage: CGFloat
     ) -> some View {
         self.modifier(
--- Sources/View Modifiers/AnimatableAngularGradient/View+AnimatableAngularGradient.swift
+++ Sources/View Modifiers/AnimatableAngularGradient/View+AnimatableAngularGradient.swift
@@ -3,13 +3,37 @@ import SwiftUI
 
 extension View {
     
+    /// - Parameters:
+    ///   - fullSpanStartAngle: Setting this property will cause the gradient to being at
+    ///         the specified offset, and then span the entire circumference. (Default: `nil`)
     public func animatableAngularGradient<BaseShape: Shape>(
         baseShape: BaseShape,
         startColors: [UIColor],
         endColors: [UIColor],
         centerPoint: UnitPoint = .center,
-        startAngle: Angle,
-        endAngle: Angle,
+        fullSpanStartAngle: Angle,
+        completionPercentage: CGFloat
+    ) -> some View {
+        self.modifier(
+            AnimatableAngularGradientModifier(
+                baseShape: baseShape,
+                startColors: startColors,
+                endColors: endColors,
+                centerPoint: centerPoint,
+                fullSpanStartAngle: fullSpanStartAngle,
+                completionPercentage: completionPercentage
+            )
+        )
+    }
+    
+    
+    public func animatableAngularGradient<BaseShape: Shape>(
+        baseShape: BaseShape,
+        startColors: [UIColor],
+        endColors: [UIColor],
+        centerPoint: UnitPoint = .center,
+        startAngle: Angle = .zero,
+        endAngle: Angle = .radians(2 * .pi),
         completionPercentage: CGFloat
     ) -> some View {
         self.modifier(
